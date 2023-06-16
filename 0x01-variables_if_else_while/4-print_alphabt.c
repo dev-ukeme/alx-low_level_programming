8edbcaeff77eab1953a3f692fcb5417b307a8d74
dev-ukeme <ukemewilson3@gmail.com>
2023-06-16 16:28:06 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print letters except q and e
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char = 'a';
+
+	while (c < = 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
