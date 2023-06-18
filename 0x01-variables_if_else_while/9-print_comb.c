55b3d3db4b71d204ef56cd341149e0238ab980a4
dev-ukeme <ukemewilson3@gmail.com>
2023-06-18 13:40:22 -0700

    possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print all possible combination of 
+ * single digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i  = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return(0);
+
+}
