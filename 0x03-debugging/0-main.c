352b1f3e7b263f29a9f5d4978a37d3b15c499587
dev-ukeme <ukemewilson3@gmail.com>
2023-06-23 16:05:04 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - call positive or negative function
+ * Description: positive or negative function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 60;
+
+	positive_or_negative(i)
+
+	return (0);
+}
