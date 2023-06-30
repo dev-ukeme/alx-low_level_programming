9f740f316fab8231786232bcd273e9027991ccea
dev-ukeme <ukemewilson3@gmail.com>
2023-06-30 10:43:26 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,15 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int op = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0' && op == 0)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		op = s1[i] - s2[i];
+		if (s1[i] != s2[i])
+		{
+		return (s1[i] - s2[i]);
+		}
 		i++;
 	}
 
-	return (op);
+	return (0);
 
 }
